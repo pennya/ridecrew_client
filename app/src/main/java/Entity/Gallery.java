83a7dafe075c4988b616df32e80b964fc812efa0
KIM <rlawlgns077@gmail.com>
2018-01-16 16:44:06 +0900

    git gallery entity

---
+++ app/src/main/java/Entity/Gallery.java
@@ -0,0 +1,45 @@
+package Entity;
+
+/**
+ * Created by KIM on 2018-01-16.
+ */
+
+public class Gallery {
+    private Long id;
+    private Member member;
+    private String title;
+    private String content;
+    private String createdDateTime;
+    private String lastModifiedDateTime;
+
+    public static Gallery builder() {
+        return new Gallery();
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Gallery setMember(Member member) {
+        this.member = member;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Gallery setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Gallery setContent(String content) {
+        this.content = content;
+        return this;
+    }
+}
--- app/src/main/java/com/ridecrew/ridecrew/ui/GalleryFragment.java
+++ app/src/main/java/com/ridecrew/ridecrew/ui/GalleryFragment.java
@@ -14,6 +14,7 @@ import com.ridecrew.ridecrew.adapter.GalleryAdapter;
 
 import java.util.ArrayList;
 
+import Entity.Gallery;
 import xyz.danoz.recyclerviewfastscroller.vertical.VerticalRecyclerViewFastScroller;
 
 public class GalleryFragment extends Fragment implements View.OnClickListener {
@@ -53,59 +54,5 @@ public class GalleryFragment extends Fragment implements View.OnClickListener {
         recyclerView.setHasFixedSize(true);
 
         btnAdd.setOnClickListener(this);
-
-
-        ArrayList<GalleryEntity> items = new ArrayList<>();
-        items.add( (new GalleryEntity()).setUserId("홍길동").setLikeCount(6).setImageUrl("http://cfile1.uf.tistory.com/image/20602F0F4CEA8EE73B9543"));
-        items.add( (new GalleryEntity()).setUserId("홍길순").setLikeCount(9).setImageUrl("http://www.cj-ilbo.com/news/photo/201603/914429_18223_0003.jpg"));
-        items.add( (new GalleryEntity()).setUserId("김지훈").setLikeCount(15).setImageUrl("http://www.bicyclenews.co.kr/news/photo/201510/2246_8710_2846.jpg"));
-        items.add( (new GalleryEntity()).setUserId("이주형").setLikeCount(40).setImageUrl("http://bike-korea.com/files/attach/images/1910/874/527/4e783a1efcf8da7241a5c98cb9e927b1.jpg"));
-
-        adapter.items = items;
-        adapter.notifyDataSetChanged();
-    }
-
-    public class GalleryEntity {
-        private String iconUrl;
-        private String userId;
-        private int likeCount;
-        private String imageUrl;
-
-
-        public String getIconUrl() {
-            return iconUrl;
-        }
-
-        public GalleryEntity setIconUrl(String iconUrl) {
-            this.iconUrl = iconUrl;
-            return this;
-        }
-
-        public String getUserId() {
-            return userId;
-        }
-
-        public GalleryEntity setUserId(String userId) {
-            this.userId = userId;
-            return this;
-        }
-
-        public int getLikeCount() {
-            return likeCount;
-        }
-
-        public GalleryEntity setLikeCount(int likeCount) {
-            this.likeCount = likeCount;
-            return this;
-        }
-
-        public String getImageUrl() {
-            return imageUrl;
-        }
-
-        public GalleryEntity setImageUrl(String imageUrl) {
-            this.imageUrl = imageUrl;
-            return this;
-        }
     }
 }
