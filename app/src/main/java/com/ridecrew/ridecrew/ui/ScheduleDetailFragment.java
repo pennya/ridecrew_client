d3a0c7d5531cf4db326b4414d2de403276c88710
kim <kim@kims-MacBook-Pro.local>
2018-01-21 03:16:59 +0900

    add map in detail view

--- app/src/main/java/com/ridecrew/ridecrew/ui/ScheduleDetailFragment.java
+++ app/src/main/java/com/ridecrew/ridecrew/ui/ScheduleDetailFragment.java
@@ -3,15 +3,24 @@ package com.ridecrew.ridecrew.ui;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.NestedScrollView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.ridecrew.ridecrew.R;
 import com.ridecrew.ridecrew.presenter.ScheduleMemberPresenter;
 import com.ridecrew.ridecrew.presenter.ScheduleMemberPresenterImpl;
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapView;
 
 import java.util.List;
 
@@ -25,6 +34,8 @@ import Entity.ScheduleMember;
 import util.SharedUtils;
 import util.UtilsApp;
 
+import static com.ridecrew.ridecrew.ui.BaseTMapActivity.TMAP_API_KEY;
+
 /**
  * Created by KIM on 2018-01-03.
  */
@@ -33,26 +44,20 @@ public class ScheduleDetailFragment extends DialogFragment implements View.OnCli
 
     private ScheduleMemberPresenter presenter;
     private Schedule mCurrentSchedule;
-    private TextView mTitle;
-    private TextView mAuthor;
-    private TextView mDate;
-    private TextView mStartTime;
-    private TextView mEndTime;
-    private TextView mStartSpot;
-    private TextView mEndSpot;
-    private TextView mDescription;
+    private TextView mTitle, mAuthor, mDate, mStartTime, mEndTime, mStartSpot, mEndSpot, mDescription;
+    private Button mBtnJoin, mBtnCancel;
+    private RelativeLayout mMap;
+    private TMapView mTMapView;
+    private View mMapFake;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater dialogInfalter = getActivity().getLayoutInflater();
         View scheduleDetailView = dialogInfalter.inflate(R.layout.fragment_schedule_detail, null);
 
-        scheduleDetailView.findViewById(R.id.btn_fragment_schedule_detail_join).setOnClickListener(this);
-        scheduleDetailView.findViewById(R.id.btn_fragment_schedule_detail_modify).setOnClickListener(this);
-        scheduleDetailView.findViewById(R.id.btn_fragment_schedule_detail_cancel).setOnClickListener(this);
-
         layoutInit(scheduleDetailView);
         setDefaultSetting();
+        TMapInit();
 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
         dialogBuilder.setView(scheduleDetailView);
@@ -88,6 +93,23 @@ public class ScheduleDetailFragment extends DialogFragment implements View.OnCli
         mStartSpot = (TextView)view.findViewById(R.id.tv_fragment_schedule_detail_start_spot);
         mEndSpot = (TextView)view.findViewById(R.id.tv_fragment_schedule_detail_end_spot);
         mDescription = (TextView)view.findViewById(R.id.tv_fragment_schedule_detail_descriptions);
+
+        mBtnJoin = (Button)view.findViewById(R.id.btn_fragment_schedule_detail_join);
+        mBtnCancel = (Button)view.findViewById(R.id.btn_fragment_schedule_detail_cancel);
+
+        mMap = (RelativeLayout) view.findViewById(R.id.rl_fragment_schedule_detail_map_view);
+        mMapFake = (View) view.findViewById(R.id.v_fragment_schedule_detail_map_view_fake);
+        mBtnJoin.setOnClickListener(this);
+        mBtnCancel.setOnClickListener(this);
+
+        mMapFake.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // in order to not touch TMapView
+                // return true is required
+                return true;
+            }
+        });
     }
 
     private void setDefaultSetting() {
@@ -101,6 +123,26 @@ public class ScheduleDetailFragment extends DialogFragment implements View.OnCli
         mDescription.setText(mCurrentSchedule.getDescriptions());
 
         presenter = new ScheduleMemberPresenterImpl(this);
+    }
+
+    private void TMapInit() {
+        mTMapView = new TMapView(getActivity());
+        mTMapView.setSKPMapApiKey(TMAP_API_KEY);
+        mTMapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        mTMapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+        mTMapView.setTileType(TMapView.TILETYPE_HDTILE);
+
+        mMap.addView(mTMapView);
+
+        // 출발,도착 마커 최적화해서 보이기
+        //mTMapView.getDisplayTMapInfo(arrays);
+
+        // 출발,도착 마커
+        /*Bitmap start = BitmapFactory.decodeResource(context.getResources(),R.drawable.Start);
+        Bitmap end = BitmapFactory.decodeResource(context.getResources(),R.drawable.End);
+        Bitmap pass = BitmapFactory.decodeResource(context.getResources(),R.drawable.Pass);
+        tmapview.setTMapPathIcon(start, end, pass);*/
+
     }
 
     //다이어로그창에서 참가하기, 수정하기, 취소 터치했을 때 이벤트
