3ec288138622a44ba1d46ac8d36533d041f63cfa
JooHyeongLee <godparty@naver.com>
2018-01-22 15:32:00 +0900

    notice_animation_expand-collapse

--- app/src/main/java/com/ridecrew/ridecrew/adapter/NoticeRecyclerViewAdapter.java
+++ app/src/main/java/com/ridecrew/ridecrew/adapter/NoticeRecyclerViewAdapter.java
@@ -1,71 +1,120 @@
 package com.ridecrew.ridecrew.adapter;
 
-import android.content.Context;
+import android.animation.ValueAnimator;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.ridecrew.ridecrew.R;
 import com.ridecrew.ridecrew.callback.NoticeRecyclerViewCallback;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 import Entity.Notice;
+
 
 /**
  * Created by JooHyeong on 2018. 1. 11..
  */
 
-public class NoticeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class NoticeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private ArrayList<Notice> mItemLists;
+    private ArrayList<Boolean> mExpands;
     private NoticeRecyclerViewCallback mCallback;
-    private boolean mFlag;
+    private int mOriginalHeight = 0;
 
     public NoticeRecyclerViewAdapter(NoticeRecyclerViewCallback callback) {
         mCallback = callback;
         mItemLists = new ArrayList<>();
-        mFlag = true;
+        mExpands = new ArrayList<>();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_notice_recyclerview_item,parent,false);
+                .inflate(R.layout.fragment_notice_recyclerview_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
         final int itemPosition = position;
+        mExpands.addAll(Collections.nCopies(mItemLists.size(),Boolean.FALSE));
 
-        if(holder instanceof ViewHolder) {
-            final ViewHolder viewHolder = (ViewHolder)holder;
+        if (holder instanceof ViewHolder) {
+            final ViewHolder viewHolder = (ViewHolder) holder;
+            if(mExpands.get(itemPosition) == false) {
+                viewHolder.mConstraintLayout.setVisibility(View.VISIBLE);
+                viewHolder.mConstraintLayout.setEnabled(false);
+            }
             viewHolder.mCardView.setOnClickListener(new View.OnClickListener() {
 
                 @Override
-                public void onClick(View view) {
+                public void onClick(final View view) {
                     mCallback.showItem(itemPosition);
-                    //cardview 클릭 시 화살표 이미지 변화, contents 나타남 여부
-                    if(mFlag){
-                        viewHolder.mImgArrow.setImageResource(R.drawable.ic_action_arrow_up);
-                        viewHolder.mContents.setText(mItemLists.get(itemPosition).getContent());
+                    if (mOriginalHeight == 0) {
+                        mOriginalHeight = view.getHeight();
+                    }
+                    ValueAnimator valueAnimator;
+                    //card view가 접혀있을 때 펼치는 애니메이션
+                    if(mExpands.get(itemPosition)==false) {
                         viewHolder.mContents.setVisibility(view.VISIBLE);
-                        mFlag = false;
-                    }
-                    else {
+                        viewHolder.mImgArrow.setImageResource(R.drawable.ic_action_arrow_up);
+                        viewHolder.mCreateDateTime.setVisibility(view.VISIBLE);
+                        valueAnimator = ValueAnimator.ofInt(mOriginalHeight,mOriginalHeight+(int)(mOriginalHeight*2.0));
+                        mExpands.set(itemPosition,true);
+                    } else {    //card view가 펼쳐져 있을 때 접는 애니메이션
+                        viewHolder.mContents.setVisibility(View.INVISIBLE);
                         viewHolder.mImgArrow.setImageResource(R.drawable.ic_action_arrow_down);
-                        viewHolder.mContents.setVisibility(view.GONE);
-                        mFlag = true;
+                        valueAnimator = ValueAnimator.ofInt(mOriginalHeight + (int) (mOriginalHeight * 2.0), mOriginalHeight);
+                        Animation animation = new AlphaAnimation(1.00f, 0.00f);
+                        animation.setDuration(200);
+                        mExpands.set(itemPosition,false);
+
+                        animation.setAnimationListener(new Animation.AnimationListener() {
+
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                viewHolder.mConstraintLayout.setVisibility(View.VISIBLE);
+                                viewHolder.mConstraintLayout.setEnabled(true);
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+                            }
+                        });
+                        viewHolder.mConstraintLayout.startAnimation(animation);
                     }
+                    valueAnimator.setDuration(500);
+                    valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+                    valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            Integer value = (Integer) animation.getAnimatedValue();
+                            view.getLayoutParams().height = value.intValue();
+                            view.requestLayout();
+                        }
+                    });
+                    valueAnimator.start();
                 }
             });
             viewHolder.mTitle.setText(mItemLists.get(itemPosition).getTitle());
+            viewHolder.mContents.setText(mItemLists.get(itemPosition).getContent());
+            viewHolder.mCreateDateTime.setText(mItemLists.get(itemPosition).getCreatedDateTime());
+            viewHolder.mImgArrow.setImageResource(R.drawable.ic_action_arrow_down);
         }
     }
 
@@ -76,16 +125,24 @@ public class NoticeRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         protected CardView mCardView;
+        protected int mType;
         protected TextView mTitle;
         protected TextView mContents;
-        private ImageView mImgArrow;
+        protected TextView mImgUrl;
+        protected TextView mWebUrl;
+        protected TextView mCreateDateTime;
+        protected TextView mLastModifiedDateTime;
+        protected ImageView mImgArrow;
+        protected ConstraintLayout mConstraintLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            mCardView = (CardView)itemView.findViewById(R.id.card_view_fragment_notice_recycler_view);
-            mTitle = (TextView)itemView.findViewById(R.id.tv_fragment_notice_recycler_view_title);
-            mContents = (TextView)itemView.findViewById(R.id.tv_fragment_notice_recycler_view_contents);
-            mImgArrow = (ImageView)itemView.findViewById(R.id.img_arrow_down);
+            mCardView = (CardView) itemView.findViewById(R.id.card_view_fragment_notice_recycler_view);
+            mTitle = (TextView) itemView.findViewById(R.id.tv_fragment_notice_recycler_view_title);
+            mContents = (TextView) itemView.findViewById(R.id.tv_fragment_notice_recycler_view_contents);
+            mCreateDateTime = (TextView)itemView.findViewById(R.id.tv_fragment_notice_recycler_view_createdDateTime);
+            mImgArrow = (ImageView) itemView.findViewById(R.id.img_arrow_down);
+            mConstraintLayout = (ConstraintLayout)itemView.findViewById(R.id.cl_fragment_notice_layout);
         }
     }
 
